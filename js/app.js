b2347573dbff05383cd27994556bb84f79e54e48
Kristo Kiis <kristo@efley.ee>
2013-07-11 13:57:43 +0300

    more debugging..

--- js/app.js
+++ js/app.js
@@ -3596,6 +3596,8 @@ function uploadFile(mediaFile, isSave) {
 	    
 	    var options = new FileUploadOptions();
 	    options.fileKey="image";
+	    option.mimeType="image/jpeg";
+	    options.fileName="image.jpg";
 	    
 	    ft.onprogress = function(progressEvent) {
 		    if (progressEvent.lengthComputable) {
@@ -3611,9 +3613,9 @@ function uploadFile(mediaFile, isSave) {
 		if (app.saveStage == 1) {
 			options.params = params;
 			
-			url = app.serverUrl + "Market/updateItem/" + params.adType + "/" + params.stage + "/" + app.currentEditId + "?session=" + app.session + '&callback=123';
+			//url = app.serverUrl + "Market/updateItem/" + params.adType + "/" + params.stage + "/" + app.currentEditId + "?session=" + app.session + '&callback=123';
 			
-			//url = app.supportUrl + "?session=" + app.session + '&action=uploadFile&callback=123';
+			url = app.supportUrl + "?session=" + app.session + '&action=uploadFile&callback=123';
 			
 		} else {
 			url = app.serverUrl + "Market/addItemImage/" + app.currentEditId + "?session=" + app.session + '&callback=123';
